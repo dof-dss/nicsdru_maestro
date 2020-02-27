7015166fac20105a956bb64fe1f28ab4ed49b404
omahm <hello@onemanandhismouse.com>
2020-02-27 15:27:41 +0000

    Fetch site info from json, prompt user to select site

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace Maestro\Commands;
 
+use http\Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,7 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Exception as ConsoleException;
 
 class InstallCommand extends Command
 {
@@ -25,7 +27,27 @@ class InstallCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $filesystem = new Filesystem();
         $client = HttpClient::create();
+        $helper = $this->getHelper('question');
+
+        $content = file_get_contents('./sites.json');
+
+        $sites = json_decode($content, TRUE);
+
+        if (count($sites) > 1) {
+            $question_site = new ChoiceQuestion('Please select a site to install', array_column($sites, 'name'), 0);
+            $requested_site = $helper->ask($input, $output, $question_site);
+            $requested_site = $sites[array_search($requested_site, array_column($sites, 'name'))];
+        } elseif (count($sites) === 1) {
+            $requested_site = $sites[0];
+        } else {
+            $output->writeln('Unable to fetch site details from sites.json');
+            return 0;
+        }
+
+        $output->writeln('Installing : ' . $requested_site['name']);
+
         $response = $client->request('GET', 'https://api.github.com/repos/dof-dss/nidirect-drupal/tags');
 
         $content = $response->getContent();
@@ -37,12 +59,8 @@ class InstallCommand extends Command
             $release_names[] = $release->name;
         }
 
-        $helper = $this->getHelper('question');
         $question_release = new ChoiceQuestion('Please select a release to install', $release_names, 0);
-
         $requested_release = $helper->ask($input, $output, $question_release);
-
-        $filesystem = new Filesystem();
 
         if ($filesystem->exists($this->drupalRoot)) {
 
